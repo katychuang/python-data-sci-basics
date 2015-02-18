73637f29eee3b7d79daca51fa38afb0b92a4793c
katychuang <katychuang@gmail.com>
2015-02-18 00:28:26 -0800

    encoding

--- src/my_utils.py
+++ src/my_utils.py
@@ -12,7 +12,7 @@ import csv
 # open a file and return a double list
 def open_with_csv(filename, d='\t'):
     uuids = []
-    with open(filename) as tsvin:
+    with open(filename, encoding='utf-8') as tsvin:
         tsvin = csv.reader(tsvin, delimiter=d)
         for row in tsvin:
             uuids.append(row)
